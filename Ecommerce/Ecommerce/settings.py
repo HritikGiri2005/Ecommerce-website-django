1e1d7e937c192f24c366f2d1a958f436419aced0
HritikGiri2005 <hritikgiri2@gmail.com>
2025-08-18 14:52:53 +0530

    created models for accounts and added in settings.py

--- Ecommerce/Ecommerce/settings.py
+++ Ecommerce/Ecommerce/settings.py
@@ -72,6 +72,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Ecommerce.wsgi.application'
 
+AUTH_USER_MODEL = 'accounts.Account'
+
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
