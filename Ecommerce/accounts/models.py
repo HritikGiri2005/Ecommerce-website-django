4260d025a3e0e2c9c80bd39d47ffc5dd8ba808f4
HritikGiri2005 <hritikgiri2@gmail.com>
2025-08-18 15:38:43 +0530

    little changes and registered db

--- Ecommerce/accounts/models.py
+++ Ecommerce/accounts/models.py
@@ -26,6 +26,7 @@ class MyAccountManager(BaseUserManager):
         user = self.create_user(
             email=self.normalize_email(email),
             username=username,
+            password=password,
             first_name=first_name,
             last_name=last_name,
         )
@@ -39,6 +40,7 @@ class MyAccountManager(BaseUserManager):
 class Account(AbstractBaseUser):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50,unique=True)
+    username = models.CharField(max_length=50,unique=True)
     email = models.EmailField(max_length=100,unique=True)
     phone_number = models.CharField(max_length=50,unique=True)
 
@@ -52,7 +54,7 @@ class Account(AbstractBaseUser):
     is_superadmin = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username','first_name',last_name]
+    REQUIRED_FIELDS = ['username','first_name','last_name']
 
     objects = MyAccountManager()
 
