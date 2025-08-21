70e274691385d07a23937763ef8741f3f5d44506
HritikGiri2005 <hritikgiri2@gmail.com>
2025-08-21 21:06:12 +0530

    changed views.py again and added for loop in home.html

--- Ecommerce/Ecommerce/views.py
+++ Ecommerce/Ecommerce/views.py
@@ -5,6 +5,6 @@ def home(request):
     products = Product.objects.all().filter(is_available = True)
 
     context = {
-        'products' = products
+        'products' : products
     }
     return render(request,'home.html',context)
