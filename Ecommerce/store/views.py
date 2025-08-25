0158a0de25510c8e2698d4d108fd8112e4b90cd9
HritikGiri2005 <hritikgiri2@gmail.com>
2025-08-25 20:59:04 +0530

    added product_count variable to views

--- Ecommerce/store/views.py
+++ Ecommerce/store/views.py
@@ -4,6 +4,9 @@ from .models import Product
 # Create your views here.
 def store(request):
     products = Product.objects.all().filter(is_available = True)
+    product_count = products.count()
+
+
 
     context = {
         'products' : products,
