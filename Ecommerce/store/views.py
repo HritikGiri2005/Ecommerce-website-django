2c38912ed4c531eda0fadaed9c34e70215b624d9
HritikGiri2005 <hritikgiri2@gmail.com>
2025-08-26 17:31:22 +0530

    added category functionality

--- Ecommerce/store/views.py
+++ Ecommerce/store/views.py
@@ -2,8 +2,13 @@ from django.shortcuts import render
 from .models import Product
 
 # Create your views here.
-def store(request):
-    products = Product.objects.all().filter(is_available = True)
+def store(request, category_slug = None):
+    if category_slug:
+        # filter products by category
+        products = Product.objects.filter(category__slug=category_slug)
+    else:
+        # show all products
+        products = Product.objects.all()
     product_count = products.count()
 
 
